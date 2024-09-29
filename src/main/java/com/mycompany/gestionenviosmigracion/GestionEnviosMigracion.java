6b4a3f88fa943e541995aea0dfa840a694b3cb1e
Felipe <felipecs5303@gmail.com>
2024-09-29 17:00:29 -0500

    Creacion Proyecto de un sistema de infomracion en Java y SQL Server que permita la gestion de envios de tecnologia entre oficinas de Migracion Colombia

---
+++ src/main/java/com/mycompany/gestionenviosmigracion/GestionEnviosMigracion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionenviosmigracion;
+
+/**
+ *
+ * @author Felipe
+ */
+public class GestionEnviosMigracion {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto hecho por Felipe Castellanos y Nicolas Romero");
+    }
+}
